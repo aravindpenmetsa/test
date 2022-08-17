cfaa7398f0db3068af6d63c49c2dc716d015cc7e
Aravind Penmetsa <coolaravindster@gmail.com>
2022-08-17 16:25:44 +0530

    converting the images

--- test_src/test_changeImage.py
+++ test_src/test_changeImage.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
 
+import test_constants
 import unittest
-import changeImage
+from changeImage import change_ext
+from changeImage import convert_image_to_JPEG
 
 class TestChangePath(unittest.TestCase):
 
     def test_change_ext(self):
         #Valid input
         self.assertEqual(
-        change_ext('/abc/xyz/123.txt', 'jpg'), '/abc/xyz/123.jpg')
+        change_ext('/abc/xyz/123.txt', '.jpg'), '/abc/xyz/123.jpg')
         # Empty string replacement
         self.assertEqual(
         change_ext('/abc/xyz/123.txt', ''), '/abc/xyz/123')
         #Invalid inputs
-        # No Ext
-        self.assertRaises(ValueError
-        change_ext('/abc/xyz/123', 'jpg'))
         # No source path
-        self.assertRaises(ValueError
-        change_ext('', 'jpg'), '/abc/xyz/123')
-
-
+        self.assertRaises(ValueError, change_ext, '', '.jpg')
 
     def test_convert_image(self):
-        self.assertEqual(
-        change_ext('/abc/xyz/123.txt', 'jpg'), '/abc/xyz/123.jpg')
+        self.assertTrue(convert_image_to_JPEG(test_constants.tiff_imgs_test_data_dir + "01.tiff"))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_src/test_constants.py
@@ -0,0 +1,1 @@
+tiff_imgs_test_data_dir = "/home/aravind508/dev_workspaces/python/coursera_python_scripts/final_proj/test/test_data/tiff_images/"
